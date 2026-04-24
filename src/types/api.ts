0165229542ece210f8247716685cb9d8d7aef58d
GersonNiandrie <Gniandrie@gmail.com>
2026-04-24 11:14:48 +0700

    fix: now can delete category from category page and auto-updates

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,11 +1,13 @@
+export type PaginationMeta = {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+};
+
 export type ApiResponse<T> = {
   success: boolean;
   message: string;
   data: T;
-  meta?: {
-    page: number;
-    limit: number;
-    total: number;
-    totalPages: number;
-  };
+  meta?: PaginationMeta;
 };
